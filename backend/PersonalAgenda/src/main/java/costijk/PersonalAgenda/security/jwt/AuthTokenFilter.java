d03c5cdf0db6758ebafd636a5819d12f23a079f6
Costijk <119739051+Costijk@users.noreply.github.com>
2025-12-24 15:41:10 +0200

    security

---
+++ backend/PersonalAgenda/src/main/java/costijk/PersonalAgenda/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,56 @@
+package costijk.PersonalAgenda.security.jwt;
+
+import costijk.PersonalAgenda.model.User;
+import costijk.PersonalAgenda.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+
+            String jwt = parseJwt(request);
+            if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+                String userId = jwtUtils.getIdFromJwtToken(jwt);
+                User userDetails = userRepository.findById(userId)
+                        .orElseThrow(() -> new RuntimeException("User not found in token validation."));
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception e) {
+            log.error("Cannot set user authentication: {}", e.getMessage());
+        }
+
+        filterChain.doFilter(request, response);
+    }
+    private String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7);
+        }
+        return null;
+    }
+}
---
+++ backend/PersonalAgenda/src/main/java/costijk/PersonalAgenda/service/AuthService.java
@@ -0,0 +1,51 @@
+package costijk.PersonalAgenda.service;
+
+import costijk.PersonalAgenda.dto.JwtResponse;
+import costijk.PersonalAgenda.dto.LoginRequest;
+import costijk.PersonalAgenda.dto.RegisterRequest;
+import costijk.PersonalAgenda.model.User;
+import costijk.PersonalAgenda.repository.UserRepository;
+import costijk.PersonalAgenda.security.jwt.JwtUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtils jwtUtils;
+
+    public void registerUser(RegisterRequest registerRequest) {
+        if (userRepository.existsByEmail(registerRequest.getEmail())) {
+            throw new RuntimeException("Error: Email is already in use!");
+        }
+
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+
+        userRepository.save(user);
+    }
+
+
+    public JwtResponse authenticateUser(LoginRequest loginRequest) {
+
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+        User userDetails = (User) authentication.getPrincipal();
+        return new JwtResponse(jwt, "Bearer", userDetails.getId(), userDetails.getEmail());
+    }
+}
